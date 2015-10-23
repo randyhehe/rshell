5efab9d35c135ade7d7288e112bc068d1dc55a3a
randyhehe <rhe004@ucr.edu>
2015-10-22 17:42:34 -0700

    Works with passed in parameters using execvp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,58 @@
 #include <vector>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <sys/wait.h>
 
 std::vector<std::string> splitString(std::string str);
 
-int main()
+int main(int argc, char** argv)
 {
-    std::string userInput;
-    
-    for(;;)
+    char* args[argc];
+    for(int i = 1; i < argc; i++)
     {
-        std::getline(std::cin, userInput);
-        if(userInput == "exit")
-            exit(0);
+        args[i - 1] = argv[i];
+    }
+    args[argc - 1] = NULL;
 
-        std::vector<std::string> vecWords = splitString(userInput);
+    pid_t processID;
+    int status;
+
+    processID = fork();
+    
+    if(processID < 0)
+    {
+        perror("fork failed");
+        exit(1);
+    }
+
+    else if(processID == 0)
+    {
+        execvp(args[0], args);
+
+        for(int i = 0; i < argc - 1; i++)
+        {
+            std::cout << args[i] << std::endl;
+        }
+
+        perror("execvp failed");
+    }
+    else
+    {
+        if((processID = wait(&status)) < 0)
+        {
+            perror("wait");
+            exit(1);
+        }
+
+        std::cout << "hello" << std::endl;
     }
 
     return 0;
 }
 
+// Splits a string of sentences to a vector of words
 std::vector<std::string> splitString(std::string str)
 {
     std::vector<std::string> vectorWords;
